3ad1b5f7e20b9767746ba52630ff2956d6aa4703
Navami Naveen Borkar <139858914+navamiborkar@users.noreply.github.com>
2025-03-13 15:58:56 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, PES2UG22CS343\n";
+  cout << "Hello, PES2UG22CS343-NAVAMI\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
